8bdbab133d6eb78187b36973d058178ddfdcc399
saksham.s <sakshams982@gmail.com>
2024-02-23 21:04:49 +0545

    created a Task model

--- todo/base/models.py
+++ todo/base/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
 # Create your models here.
+class Task(models.Model):
+
+    user=models.ForeignKey(User, on_delete=models.CASCADE,null=True,blan=True)
+    title=models.CharField( max_length=50)
+    description=models.TextField(null=True,blank=True)
+    complete=models.BooleanField(default=True)
+    create=models.DateTimeField(auto_add_now=True)
+
+    class Meta:
+      ordering=['complete']
+
+    def __str__(self):
+        return self.title
+
+    # def get_absolute_url(self):
+    #     return reverse("_detail", kwargs={"pk": self.pk})
