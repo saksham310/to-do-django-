d93609fc5b30dccf708331cd6a6340b97a40f5b8
saksham.s <sakshams982@gmail.com>
2024-02-24 14:33:15 +0545

    added styling and create,update functions

--- todo/base/urls.py
+++ todo/base/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.taskList,name="home")
+    path("",views.taskList,name="home"),
+    path("update/<str:pk>",views.taskUpdate,name="updateTask"),
 ]
--- todo/base/views.py
+++ todo/base/views.py
@@ -1,9 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from . models import Task
+from . forms import TaskForm
 # Create your views here.
 def taskList(request):
     context={}
     data=Task.objects.all()
+    form=TaskForm()
+    if request.method=="POST":
+        form=TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+            
     context['data']=data
+    context['form']=form
     return render(request,'base/task_list.html',context)
+
+def taskUpdate(request,pk):
+    obj=Task.objects.get(id=pk)
+    context={}
+    form=TaskForm(instance=obj)
+    if request.method=="POST":
+        form=TaskForm(request.POST,instance=obj)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+    context['form']=form
+    return render(request,'base/task_update.html',context)
